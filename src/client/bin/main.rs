134cccd62079a513081e9f516c79c295e8b17c49
AusCyber <willp@outlook.com.au>
2020-11-23 23:15:31 +1100

    Added Custom COmments

--- src/client/bin/main.rs
+++ src/client/bin/main.rs
@@ -1,4 +1,20 @@
-fn main(){
-    
+use std::net::TcpStream;
+use std::io::{Write, Read};
+use http_parser::{HttpParser, HttpParserType};
+use httptun::parser;
+use base64::decode;
+use html5ever::tendril::fmt::Slice;
 
+fn main(){
+    let mut stream = TcpStream::connect("127.0.0.1:7878").unwrap();
+    let mut aparser = http_parser::HttpParser::new(HttpParserType::Both);
+    let mut cb = parser::HttpCallback::default();
+    let get =b"GET / HTTP/1.1\r\n";
+    stream.write(get);
+    let mut buffer: [u8; 4092] = [0;4092];
+    stream.read(&mut buffer);
+   // println!("{}",String::from_utf8_lossy(&buffer));
+    aparser.execute(&mut cb,&buffer);
+    let data = cb.data();
+    println!("{} Length: {}",String::from_utf8_lossy(base64::decode(data).unwrap().as_slice()),data.len());
 }
--- src/server/bin/main.rs
+++ src/server/bin/main.rs
@@ -5,27 +5,32 @@ use std::net::{TcpListener, TcpStream};
 use std::str;
 use std::fs::File;
 use std::io::Read;
+use httptun::parser::HttpCallback;
+use http_parser::{HttpParser, HttpParserType};
+
 
 
 
 
 fn handle_connection(mut stream: TcpStream){
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
-
-    println!("Request {}",String::from_utf8_lossy(&buffer[..]));
     let filedat = base64::encode(get_file_as_byte(&String::from("/home/auscyber/main.hs")));
+    let contents = format!("<html>\n<head><!--[{}]--></head></html>",filedat);
+    let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",contents.len(),contents);
+    stream.read(&mut buffer).unwrap();
+    println!("{}",String::from_utf8_lossy(&buffer)) ;
+    let mut parser = HttpParser::new(HttpParserType::Both);
+    let mut cb = HttpCallback::default();
+    parser.execute(&mut cb,&buffer);
+
     let get = b"GET / HTTP/1.1\r\n";
     if buffer.starts_with(get){
 
 
-    let contents = format!("<html>\n<head><!--{}--></head></html>",filedat);
-    let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",contents.len(),contents);
-
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
     }else{
-        
+
     }
 
 }
@@ -39,6 +44,8 @@ fn get_file_as_byte(filename: &String) -> Vec<u8> {
     let metadata = f.metadata().unwrap();
     let mut buffer = vec![0; metadata.len() as usize];
     f.read(&mut buffer).expect("buffer overflow lmao");
+
+
     buffer
 
 }
@@ -48,13 +55,11 @@ fn main() {
      
     
 
-    let httpListener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
-    for stream in httpListener.incoming() {
+    let http_listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    for stream in http_listener.incoming() {
         let stream = stream.unwrap();
         println!("Connection Established");
-        handle_connection(stream); 
-
+        handle_connection(stream)
     }
 }
 
